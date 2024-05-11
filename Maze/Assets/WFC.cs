7768af000e264fd339e59b950e1092d49a6fe898
martin kyles <martinkyles66@gmail.com>
2024-05-11 07:36:35 +0100

    New Tiles and Big integer

--- Maze/Assets/WFC.cs
+++ Maze/Assets/WFC.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.VisualScripting;
 using UnityEditor.SceneTemplate;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using Vector2 = UnityEngine.Vector2;
 
 
 public class WFC : MonoBehaviour
@@ -18,11 +20,11 @@ public class WFC : MonoBehaviour
     //keep equal
     [SerializeField] private int totalx = 6;
     [SerializeField] private int totaly = 6;
-    
-    
+
+    private const int totalTiles = 12; 
     private TileInfo[,] MasterTiles;
 
-    private int[,] rules;
+    private BigInteger[,] rules;
     private int[,] rulenum;
     private bool alltrue = false;
     [SerializeField] static private Texture[] Tiles;
@@ -45,7 +47,15 @@ public class WFC : MonoBehaviour
         Down =2,
         Left =4,
         Right = 8,
-        Up = 16
+        Up = 16,
+        Lup =32,
+        Ll = 64,
+        CTR = 128,
+        CTL = 256,
+        CBR = 512,
+        CBL = 1024,
+        Cross = 2048
+        
         
 
     }
@@ -77,6 +87,24 @@ public class WFC : MonoBehaviour
                 return 3;
             case 16:
                 return 4;
+            case 32:
+                return 5;
+            case 64:
+                return 6;
+            case 128:
+                return 7;
+            case 256:
+                return 8;
+            case 512:
+                return 9;
+            case 1024:
+                return 10;
+            case 2048:
+                return 11;
+            
+            
+            
+            
             default:
                 print("broke");
                 return 500;
@@ -90,7 +118,7 @@ public class WFC : MonoBehaviour
 
     struct TileInfo
     {
-        public long possibility ;
+        public BigInteger possibility ;
         public int Entropy;
         public bool Known;
 
@@ -107,9 +135,9 @@ public class WFC : MonoBehaviour
 
         public void Inst( GameObject Plane)
         {
-            possibility = (long) (TileType.Blank | TileType.Down | TileType.Left | TileType.Right | TileType.Up);
+            possibility = (long) (TileType.Cross|TileType.Blank | TileType.Down | TileType.Left | TileType.Right | TileType.Up | TileType.Ll | TileType.Lup | TileType.CBL | TileType.CBR | TileType.CTL | TileType.CTR);
            // print( possibility);
-            Entropy = 5;
+            Entropy = totalTiles;
             Known = false;
             plane = Plane;
             mat = plane.GetComponent<Renderer>().material;
@@ -135,51 +163,129 @@ public class WFC : MonoBehaviour
     }
 
 
-    int[,] initrules( out int[,] rulenume )
+    BigInteger[,] initrules()
     {
-        int[,] a = new int[5,4];
-        rulenume = new int[5,4];
+        BigInteger[,] a = new  BigInteger[totalTiles,4];
+       //remove blank
 
-        a[TPowerReverse(TileType.Blank), (int)Direction.Up] = (int) (TileType.Blank | TileType.Up);
+        a[TPowerReverse(TileType.Blank), (int)Direction.Up] = (int) (TileType.Blank | TileType.Up |TileType.Down | TileType.Left | TileType.Right |TileType.Ll | TileType.Lup | TileType.Up |TileType.CBL | TileType.CBR | TileType.CTL |TileType.CTR | TileType.Cross);
      
-        a[TPowerReverse(TileType.Blank), (int)Direction.Left] = (int) (TileType.Blank | TileType.Left);
+        a[TPowerReverse(TileType.Blank), (int)Direction.Left] =(int) (TileType.Blank | TileType.Up |TileType.Down | TileType.Left | TileType.Right |TileType.Ll | TileType.Lup | TileType.Up |TileType.CBL | TileType.CBR | TileType.CTL |TileType.CTR | TileType.Cross);
+
+        a[TPowerReverse(TileType.Blank), (int)Direction.Down] = (int) (TileType.Blank | TileType.Up |TileType.Down | TileType.Left | TileType.Right |TileType.Ll | TileType.Lup | TileType.Up |TileType.CBL | TileType.CBR | TileType.CTL |TileType.CTR | TileType.Cross);
+
+        a[TPowerReverse(TileType.Blank), (int)Direction.Right] = (int) (TileType.Blank | TileType.Up |TileType.Down | TileType.Left | TileType.Right |TileType.Ll | TileType.Lup | TileType.Up |TileType.CBL | TileType.CBR | TileType.CTL |TileType.CTR | TileType.Cross);
+
+        //
+        a[TPowerReverse(TileType.Down), (int)Direction.Up] = (int) (TileType.Blank |TileType.Ll | TileType.Up | TileType.CBL | TileType.CBR );
+     
+        a[TPowerReverse(TileType.Down), (int)Direction.Left] = (int) (TileType.Right | TileType.Up | TileType.Down | TileType.CTR | TileType.CBR | TileType.Down | TileType.Ll| TileType.Cross );
+      
+        a[TPowerReverse(TileType.Down), (int)Direction.Down] = (int) (TileType.Lup | TileType.Up | TileType.CBL | TileType.CBR | TileType.Left | TileType.Right| TileType.Cross );
+      
+        a[TPowerReverse(TileType.Down), (int)Direction.Right] = (int) (TileType.Left | TileType.Up | TileType.Down | TileType.CTL |TileType.CBL | TileType.Ll | TileType.Cross);
+       //
        
-        a[TPowerReverse(TileType.Blank), (int)Direction.Down] = (int) (TileType.Blank | TileType.Down);
+        a[TPowerReverse(TileType.Right), (int)Direction.Up] = (int) (TileType.Left | TileType.Down | TileType.Right | TileType.Lup | TileType.CTL | TileType.CTR | TileType.Cross);
+     
+        a[TPowerReverse(TileType.Right), (int)Direction.Left] = (int) (TileType.Blank | TileType.Left | TileType.Lup | TileType.CTL | TileType.CBL);
        
-        a[TPowerReverse(TileType.Blank), (int)Direction.Right] = (int) (TileType.Blank | TileType.Right);
+        a[TPowerReverse(TileType.Right), (int)Direction.Down] = (int) (TileType.Left | TileType.Up | TileType.Right | TileType.Lup | TileType.CBR | TileType.CBL | TileType.Cross);
+       
+        a[TPowerReverse(TileType.Right), (int)Direction.Right] = (int) ( TileType.Down | TileType.Up | TileType.Left | TileType.Ll  | TileType.CTL | TileType.CTR | TileType.Cross);
+        //
+        a[TPowerReverse(TileType.Left), (int)Direction.Up] = (int) (TileType.Left | TileType.Down | TileType.Right | TileType.Lup | TileType.CTL | TileType.CTR | TileType.Cross);
+     
+        a[TPowerReverse(TileType.Left), (int)Direction.Left] = (int) (TileType.Up | TileType.Down | TileType.Right | TileType.Ll | TileType.CTR | TileType.CBR | TileType.Cross);
+      
+        a[TPowerReverse(TileType.Left), (int)Direction.Down] = (int) (TileType.Left | TileType.Right | TileType.Up | TileType.Lup | TileType.CBR | TileType.CBL | TileType.Cross);
+      
+        a[TPowerReverse(TileType.Left), (int)Direction.Right] = (int) (TileType.Blank |TileType.Blank | TileType.Right | TileType.CTR | TileType.CBR | TileType.Lup);
+        //
         
-        a[TPowerReverse(TileType.Down), (int)Direction.Up] = (int) (TileType.Blank | TileType.Up );
-     
-        a[TPowerReverse(TileType.Down), (int)Direction.Left] = (int) (TileType.Right | TileType.Up | TileType.Down);
-      
-        a[TPowerReverse(TileType.Down), (int)Direction.Down] = (int) (TileType.Left | TileType.Right | TileType.Up);
-      
-        a[TPowerReverse(TileType.Down), (int)Direction.Right] = (int) (TileType.Left | TileType.Up | TileType.Down);
-       
-        a[TPowerReverse(TileType.Right), (int)Direction.Up] = (int) (TileType.Left | TileType.Down | TileType.Right);
-     
-        a[TPowerReverse(TileType.Right), (int)Direction.Left] = (int) (TileType.Blank | TileType.Left);
-       
-        a[TPowerReverse(TileType.Right), (int)Direction.Down] = (int) (TileType.Left | TileType.Up | TileType.Right);
-       
-        a[TPowerReverse(TileType.Right), (int)Direction.Right] = (int) ( TileType.Down | TileType.Up | TileType.Left);
-        
-        a[TPowerReverse(TileType.Left), (int)Direction.Up] = (int) (TileType.Left | TileType.Down | TileType.Right);
-     
-        a[TPowerReverse(TileType.Left), (int)Direction.Left] = (int) (TileType.Up | TileType.Down | TileType.Right);
-      
-        a[TPowerReverse(TileType.Left), (int)Direction.Down] = (int) (TileType.Left | TileType.Right | TileType.Up);
-      
-        a[TPowerReverse(TileType.Left), (int)Direction.Right] = (int) (TileType.Blank | TileType.Right);
-        
-        a[TPowerReverse(TileType.Up), (int)Direction.Up] = (int) (TileType.Left | TileType.Down | TileType.Right);
+        a[TPowerReverse(TileType.Up), (int)Direction.Up] = (int) (TileType.Left | TileType.Down | TileType.Right | TileType.Lup | TileType.CTL | TileType.CTR | TileType.Cross );
    
-        a[TPowerReverse(TileType.Up), (int)Direction.Left] = (int) (TileType.Up | TileType.Down | TileType.Right);
+        a[TPowerReverse(TileType.Up), (int)Direction.Left] = (int) (TileType.Up | TileType.Down | TileType.Right | TileType.Ll | TileType.CTR | TileType.CBR | TileType.Cross);
      
-        a[TPowerReverse(TileType.Up), (int)Direction.Down] = (int) (TileType.Blank | TileType.Down);
+        a[TPowerReverse(TileType.Up), (int)Direction.Down] = (int) (TileType.Blank | TileType.Down | TileType.Ll | TileType.CTL | TileType.CTR);
      
-        a[TPowerReverse(TileType.Up), (int)Direction.Right] = (int) (TileType.Up | TileType.Down | TileType.Left);
+        a[TPowerReverse(TileType.Up), (int)Direction.Right] = (int) (TileType.Up | TileType.Down | TileType.Left | TileType.Ll | TileType.CTL | TileType.CBL | TileType.Cross);
      
+        //
+        a[TPowerReverse(TileType.Lup), (int)Direction.Up] = (int) (TileType.Blank |TileType.Left | TileType.Down | TileType.Right | TileType.CTL | TileType.CTR | TileType.Lup | TileType.Cross);
+   
+        a[TPowerReverse(TileType.Lup), (int)Direction.Left] = (int) ( TileType.Left | TileType.CBL | TileType.CTL | TileType.Lup);
+     
+        a[TPowerReverse(TileType.Lup), (int)Direction.Down] = (int) (TileType.Blank|TileType.Up | TileType.Left | TileType.Right | TileType.CBR | TileType.CBL | TileType.Lup | TileType.Cross);
+     
+        a[TPowerReverse(TileType.Lup), (int)Direction.Right] = (int) (TileType.Right | TileType.CBR | TileType.CTR | TileType.Lup);
+
+        //
+        
+        a[TPowerReverse(TileType.Ll), (int)Direction.Up] = (int) (TileType.Up | TileType.Ll | TileType.CBL | TileType.CBR);
+   
+        a[TPowerReverse(TileType.Ll), (int)Direction.Left] = (int) (TileType.Blank |TileType.Up | TileType.Down | TileType.Right | TileType.Ll | TileType.CTR | TileType.CBR  | TileType.Cross);
+     
+        a[TPowerReverse(TileType.Ll), (int)Direction.Down] = (int) (TileType.Down | TileType.Ll | TileType.CTL | TileType.CTR);
+     
+        a[TPowerReverse(TileType.Ll), (int)Direction.Right] = (int) (TileType.Blank |TileType.Blank |TileType.Up | TileType.Down | TileType.Left | TileType.Ll | TileType.CTL | TileType.CBL | TileType.Cross);
+
+        //
+        
+        a[TPowerReverse(TileType.CBL), (int)Direction.Up] = (int) (TileType.Left | TileType.Down | TileType.Right | TileType.Lup | TileType.CTR | TileType.CTL | TileType.Cross );
+   
+        a[TPowerReverse(TileType.CBL), (int)Direction.Left] = (int) (TileType.Right | TileType.Down | TileType.Up | TileType.Ll | TileType.CTR | TileType.CBR | TileType.Cross);
+     
+        a[TPowerReverse(TileType.CBL), (int)Direction.Down] = (int) ( TileType.Down | TileType.Ll | TileType.CTR | TileType.CTL);
+     
+        a[TPowerReverse(TileType.CBL), (int)Direction.Right] = (int) (TileType.Right |  TileType.Up | TileType.CBR | TileType.CTR);
+
+        //
+        
+        a[TPowerReverse(TileType.CBR), (int)Direction.Up] = (int) (TileType.Left | TileType.Down | TileType.Right | TileType.Lup | TileType.CTR | TileType.CTL | TileType.Cross);
+   
+        a[TPowerReverse(TileType.CBR), (int)Direction.Left] = (int) (TileType.Right | TileType.Down | TileType.Up | TileType.Ll | TileType.CTR | TileType.CBR);
+     
+        a[TPowerReverse(TileType.CBR), (int)Direction.Down] = (int) (TileType.Down | TileType.Ll | TileType.CTR | TileType.CTL);
+     
+        a[TPowerReverse(TileType.CBR), (int)Direction.Right] = (int) (TileType.Left |  TileType.Up  |  TileType.Down| TileType.Ll | TileType.CBL | TileType.CTL | TileType.Cross);
+
+        
+        //
+        
+        a[TPowerReverse(TileType.CTL), (int)Direction.Up] = (int) ( TileType.Up | TileType.Ll | TileType.CBR | TileType.CBL);
+   
+        a[TPowerReverse(TileType.CTL), (int)Direction.Left] = (int) (TileType.Right | TileType.Down | TileType.Up | TileType.Ll | TileType.CTR | TileType.CBR | TileType.Cross);
+     
+        a[TPowerReverse(TileType.CTL), (int)Direction.Down] = (int) (TileType.Up | TileType.Left | TileType.Right | TileType.Lup | TileType.CBR | TileType.CBL | TileType.Cross);
+     
+        a[TPowerReverse(TileType.CTL), (int)Direction.Right] = (int) (TileType.Right | TileType.Lup | TileType.CTR | TileType.CBR);
+
+        //
+        
+        a[TPowerReverse(TileType.CTR), (int)Direction.Up] = (int) (TileType.Up  | TileType.Ll | TileType.CBR | TileType.CBL);
+   
+        a[TPowerReverse(TileType.CTR), (int)Direction.Left] = (int) (TileType.Left | TileType.Lup | TileType.CTL | TileType.CBL);
+     
+        a[TPowerReverse(TileType.CTR), (int)Direction.Down] = (int) (TileType.Up | TileType.Left | TileType.Right | TileType.Lup | TileType.CBR | TileType.CBL | TileType.Cross);
+     
+        a[TPowerReverse(TileType.CTR), (int)Direction.Right] = (int) (TileType.Left | TileType.Down | TileType.Up | TileType.Ll | TileType.CTL | TileType.CBL | TileType.Cross);
+
+        
+        //
+        a[TPowerReverse(TileType.Cross), (int)Direction.Up] = (int) (TileType.Down | TileType.Left | TileType.Right  | TileType.Lup | TileType.CTR | TileType.CTL | TileType.Cross);
+   
+        a[TPowerReverse(TileType.Cross), (int)Direction.Left] = (int) (TileType.Right | TileType.Down | TileType.Up |TileType.Ll | TileType.Cross | TileType.CBR | TileType.CTR);
+     
+        a[TPowerReverse(TileType.Cross), (int)Direction.Down] = (int) (TileType.Up | TileType.Left | TileType.Right  | TileType.Lup | TileType.CBR | TileType.CBL | TileType.Cross);
+     
+        a[TPowerReverse(TileType.Cross), (int)Direction.Right] = (int) (TileType.Left | TileType.Down | TileType.Up |TileType.Ll | TileType.Cross | TileType.CBL | TileType.CTL | TileType.Cross);
+
+        
+        
+        
+        
+        
         return a;
     }
 
@@ -191,7 +297,7 @@ public class WFC : MonoBehaviour
        // print("HERERERE"+
            // (int)(TileType.Blank | TileType.Down | TileType.Left | TileType.Right | TileType.Up));
         Tiles = TTemp;
-        rules = initrules(out rulenum);
+        rules = initrules();
         
         
 
@@ -222,17 +328,17 @@ public class WFC : MonoBehaviour
 
         int sx = Random.Range(0, totalx);
         int sy = Random.Range(0, totaly);
-        int TT = Random.Range(0, 5);
+        int TT = Random.Range(0, totalTiles);
         MasterTiles[sx, sy].Known = true;
         MasterTiles[sx, sy].Entropy = 1;
         MasterTiles[sx, sy].possibility = (MasterTiles[sx, sy].possibility & (int)1<<TT);
         MasterTiles[sx,sy].IntetoImage(TT);
 
        
-        UpdateNeighbour(1, 0, Direction.Right, (sx,sy), TT);
-        UpdateNeighbour(-1, 0, Direction.Left, (sx,sy), TT);
-        UpdateNeighbour(0, 1, Direction.Up, (sx,sy), TT);
-        UpdateNeighbour(0, -1, Direction.Down, (sx,sy), TT);
+       UpdateNeighbour(1, 0, Direction.Right, (sx,sy), TT);
+       UpdateNeighbour(-1, 0, Direction.Left, (sx,sy), TT);
+       UpdateNeighbour(0, 1, Direction.Up, (sx,sy), TT);
+       UpdateNeighbour(0, -1, Direction.Down, (sx,sy), TT);
        
     }
 
@@ -260,34 +366,34 @@ public class WFC : MonoBehaviour
     //do better
     int nuentropy(int x, int y)
     {
-        // int count = 0;
-        // int num = MasterTiles[x, y].possibility;
-        // //automate this
-        // for (int i = 0; i < 5; i++)
-        // {
-        //     if ((num & 1) == 1)
-        //     {
-        //         count++;
+        int count = 0;
+        BigInteger num = MasterTiles[x, y].possibility;
+        //automate this
+        for (int i = 0; i < totalTiles; i++)
+        {
+            if ((num & 1) == 1)
+            {
+                count++;
+        
+            }
+            num = num >> 1;
+        
+        }
+        
+        return count;
+        //  BigInteger xi = MasterTiles[x, y].possibility;
         //
-        //     }
-        //     num = num >> 1;
         //
-        // }
         //
-        // return count;
-         long xi = MasterTiles[x, y].possibility;
-        
-        
-        
-        // https://en.wikipedia.org/wiki/Hamming_weight#:~:text=The%20Hamming%20weight%20of%20a,string%20of%20the%20same%20length.
-        
-            xi = (xi & m1 ) + ((xi >>  1) & m1 ); //put count of each  2 bits into those  2 bits 
-            xi = (xi & m2 ) + ((xi >>  2) & m2 ); //put count of each  4 bits into those  4 bits 
-            xi = (xi & m4 ) + ((xi >>  4) & m4 ); //put count of each  8 bits into those  8 bits 
-            xi = (xi & m8 ) + ((xi >>  8) & m8 ); //put count of each 16 bits into those 16 bits 
-            xi = (xi & m16) + ((xi >> 16) & m16); //put count of each 32 bits into those 32 bits 
-            xi = (xi & m32) + ((xi >> 32) & m32); //put count of each 64 bits into those 64 bits 
-            return (int)xi;
+        // // https://en.wikipedia.org/wiki/Hamming_weight#:~:text=The%20Hamming%20weight%20of%20a,string%20of%20the%20same%20length.
+        //
+        //     xi = (xi & m1 ) + ((xi >>  1) & m1 ); //put count of each  2 bits into those  2 bits 
+        //     xi = (xi & m2 ) + ((xi >>  2) & m2 ); //put count of each  4 bits into those  4 bits 
+        //     xi = (xi & m4 ) + ((xi >>  4) & m4 ); //put count of each  8 bits into those  8 bits 
+        //     xi = (xi & m8 ) + ((xi >>  8) & m8 ); //put count of each 16 bits into those 16 bits 
+        //     xi = (xi & m16) + ((xi >> 16) & m16); //put count of each 32 bits into those 32 bits 
+        //     xi = (xi & m32) + ((xi >> 32) & m32); //put count of each 64 bits into those 64 bits 
+        //     return (int)xi;
         
         
         
@@ -331,14 +437,21 @@ public class WFC : MonoBehaviour
         do
         {
             ++count;
-            tt = Random.Range(0, 5);
+            tt = Random.Range(0, totalTiles);
            // print("Heeeeeee");
            // print("t"+tt);
             //print("binar"+ (1 << tt));
            // print("pos"+MasterTiles[coord.Item1, coord.Item2].possibility);
-        } while (((1<<tt) & MasterTiles[coord.Item1, coord.Item2].possibility) == 0 && count < 100);
+         
+          
+        } while (((BigInteger)(1<<tt) & MasterTiles[coord.Item1, coord.Item2].possibility) == 0 && count < 100);
 
-       // print(count);
+        if (count == 100)
+        {
+            
+            print("BROOOOOOOOOOKKKKKKKKKKKEEEEEEEEE");
+            // print(count);
+        }
         return tt;
 
 
@@ -395,6 +508,7 @@ public class WFC : MonoBehaviour
               
 
                 int TT = targetIndex(tind);
+//                print("TTTERRR: " + TT );
                
                 MasterTiles[tind.Item1, tind.Item2].Known = true;
                 MasterTiles[tind.Item1, tind.Item2].Entropy = 1;
@@ -414,14 +528,15 @@ public class WFC : MonoBehaviour
 
                 alltrue = done();
                 TempTimer = Timer;
+              
 
             }
 
            TempTimer -= Time.deltaTime;
 
 
+         
 
-        
 
 
 
