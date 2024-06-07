3aacd2a4da4a97344a26891ec8282edfeac677d8
martin kyles <martinkyles66@gmail.com>
2024-06-07 21:05:14 +0100

    binary file

--- Maze/Assets/Store.cs
+++ Maze/Assets/Store.cs
@@ -1,12 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Store : MonoBehaviour
 {
+    private const string fileLoc = "D:/Game projects/untiy_Maze/Maze/Assets/SaveFile/yaya.bin";
+
+    void AddtoFile(int loc)
+    {
+        using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(fileLoc, FileMode.OpenOrCreate)))
+        {
+            for (int i = 0; i < loc; i++)
+            {
+                int r = (int)Random.Range(0, Int32.MaxValue);
+                binaryWriter.Write(r);
+                
+            }
+
+            
+            
+            print("Wrotten");
+            
+        }
+
+
+    }
+
+
+    int[] readFromFile(int loc)
+    {
+        int[] rturn = new int[loc];
+        using (BinaryReader binaryReader = new BinaryReader(File.Open(fileLoc, FileMode.OpenOrCreate)))
+        {
+            for (int i = 0; i < loc; i++)
+            {
+
+                rturn[i] = binaryReader.ReadInt32();
+            }
+
+            print("readen");
+            
+        }
+
+        return rturn;
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        AddtoFile(23);
+        int[] a = (readFromFile(23));
+
+
+        foreach (var VARIABLE in a)
+        {
+            print(VARIABLE);
+        }
         
     }
 
