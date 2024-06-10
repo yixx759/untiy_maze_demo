60a73b2fedb7943b001c9d39e8228a88f1a19bb6
martin kyles <martinkyles66@gmail.com>
2024-06-10 22:48:17 +0100

    maze file

--- Maze/Assets/Store.cs
+++ Maze/Assets/Store.cs
@@ -9,7 +9,7 @@ using Random = UnityEngine.Random;
 public class Store : MonoBehaviour
 {
     private const string fileLoc = "D:/Game projects/untiy_Maze/Maze/Assets/SaveFile/yaya.bin";
-
+//have xy then jump to next of same sighn eg 3,1 4 the next 4,1 will be in 4 jumps, over write when writing next pos or neg
     void AddtoFile(int loc)
     {
         using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(fileLoc, FileMode.OpenOrCreate)))
@@ -32,40 +32,58 @@ public class Store : MonoBehaviour
 //parraeleize test please
     void AppendtoFile(int loc)
     {
-        int[] rturn = new int[loc];
-        using (BinaryReader binaryReader = new BinaryReader(File.Open(fileLoc, FileMode.OpenOrCreate)))
-        {
-            for (int i = 0; i < loc; i++)
-            {
+        // int[] rturn = new int[loc];
+        // using (BinaryReader binaryReader = new BinaryReader(File.Open(fileLoc, FileMode.OpenOrCreate)))
+        // {
+        //     for (int i = 0; i < loc; i++)
+        //     {
+        //
+        //         rturn[i] = binaryReader.ReadInt32();
+        //     }
+        //
+        //     print("readen");
+        //     
+        // }
+        //
+        //
+        // using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(fileLoc, FileMode.OpenOrCreate)))
+        // {
+        //     
+        //     binaryWriter.Write(1920);
+        //   //  Span<byte> bytes = MemoryMarshal.Cast<int, byte>(nurturn.AsSpan());
+        //   for (int i = 0; i < loc; i++)
+        //   {
+        //       binaryWriter.Write(rturn[i]);    
+        //   }
+        //     
+        //             
+        //     
+        //    
+        //
+        //     
+        //     
+        //     print("Wrotten");
+        //     
+        // }
 
-                rturn[i] = binaryReader.ReadInt32();
-            }
 
-            print("readen");
-            
-        }
-
-        
         using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(fileLoc, FileMode.OpenOrCreate)))
         {
-            
-            binaryWriter.Write(1920);
-          //  Span<byte> bytes = MemoryMarshal.Cast<int, byte>(nurturn.AsSpan());
-          for (int i = 0; i < loc; i++)
-          {
-              binaryWriter.Write(rturn[i]);    
-          }
-            
+            using (BinaryWriter binaryReader = new BinaryWriter(File.Open(fileLoc, FileMode.OpenOrCreate)))
+            {
+                for (int i = 0; i < loc; i++)
+                {
+                    binaryWriter.Seek(-loc * 4, SeekOrigin.End);
                     
-            
-           
 
-            
-            
-            print("Wrotten");
-            
+                }
+
+                print("readen");
+            }
         }
-        
+
+
+       
         
 
 
