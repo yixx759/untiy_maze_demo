8e72ecb7ee123b98de5b5a5d7ce7770d105ab4ad
martin kyles <martinkyles66@gmail.com>
2024-07-23 19:32:59 +0100

    res

---
+++ Maze/Assets/Res.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Res : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private RenderTexture final;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+     
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        RenderTexture.active = final;
+        Graphics.Blit(final, (RenderTexture)null);
+    }
+}
